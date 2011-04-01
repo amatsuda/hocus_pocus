395a296d42e1b9446084a748b83f919e25856783
Akira Matsuda <ronnie@dio.jp>
2011-04-01 09:36:00 +0900

    add spec clear button

---
+++ app/controllers/hocus_pocus/spec_controller.rb
@@ -0,0 +1,8 @@
+module HocusPocus
+  class SpecController < ActionController::Base
+    # XHR
+    def destroy
+      flash.discard HocusPocus::SPEC
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ HocusPocus::Engine.routes.draw do
   post '/generator/execute', :to => 'generator#execute', :as => 'execute_generator'
   get '/editor', :to => 'editor#index'
   post '/editor/save', :to => 'editor#save', :as => 'save_editor'
+  delete '/spec', :to => 'spec#destroy'
   get '/:anything', :to => 'generator#index', :constraints => {:anything => /.*/}
 end
