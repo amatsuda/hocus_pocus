428a31557273f069ccd7258ebf79572bea48c6b8
Alexandre de Oliveira <chavedomundo@gmail.com>
2011-11-24 09:17:39 -0200

    Adjusted some differences between enginex template and Rails plugin new template; database defined as in-memory.

--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -1,10 +1,6 @@
 require File.expand_path('../boot', __FILE__)
 
-require "active_model/railtie"
-require "active_record/railtie"
-require "action_controller/railtie"
-require "action_view/railtie"
-require "action_mailer/railtie"
+require "rails/all"
 
 Bundler.require
 require "hocus_pocus"
@@ -41,5 +37,8 @@ module Dummy
 
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
+
+    # Enable the asset pipeline
+    config.assets.enabled = true
   end
 end
