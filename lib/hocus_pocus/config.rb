951d5953607d9740a9d25c47e10ef63239462fc8
Akira Matsuda <ronnie@dio.jp>
2012-04-25 21:02:08 +0900

    simplify config code
    Rails 3.0 is no more supported

--- lib/hocus_pocus/config.rb
+++ lib/hocus_pocus/config.rb
@@ -1,23 +1,15 @@
 require 'active_support/configurable'
 
 module HocusPocus
+  include ActiveSupport::Configurable
+  config_accessor :enable_generator, :enable_editor, :enable_scenario_recorder, :enable_command_line
+
   # Configures global settings for HocusPocus
   #   HocusPocus.configure do |config|
   #     config.enable_generator = false
   #   end
   def self.configure(&block)
-    yield @config ||= HocusPocus::Configuration.new
-  end
-
-  # Global settings for HocusPocus
-  def self.config
-    @config
-  end
-
-  # need a Class for 3.0
-  class Configuration #:nodoc:
-    include ActiveSupport::Configurable
-    config_accessor :enable_generator, :enable_editor, :enable_scenario_recorder, :enable_command_line
+    yield self
   end
 
   # this is ugly. why can't we pass the default value to config_accessor...?
