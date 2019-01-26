34fabc32054f9e579cd913c6b07aace6531fd5c7
Akira Matsuda <ronnie@dio.jp>
2019-01-26 14:03:22 +0900

    AMC => prepend

--- engines/editor/lib/hocus_pocus/editor/railtie.rb
+++ engines/editor/lib/hocus_pocus/editor/railtie.rb
@@ -6,6 +6,20 @@ module HocusPocus
     VIEW_FILENAME = :__hocus_pocus_view_filename__
     PARTIAL_FILENAMES = :__hocus_pocus_partial_filenames__
 
+    module PartialRendererExtension
+      def render_partial
+        (Thread.current[HocusPocus::Editor::PARTIAL_FILENAMES] ||= []) << @template unless @view.controller.class.name.starts_with?('HocusPocus::')
+        super
+      end
+    end
+
+    module TemplateRendererExtension
+      def render_template(template, layout_name = nil, locals = {})
+        Thread.current[HocusPocus::Editor::VIEW_FILENAME] = template.virtual_path if @view.controller.try(:request).try(:format).try(:html?) && !@view.controller.class.name.starts_with?('HocusPocus::')
+        super
+      end
+    end
+
     class Railtie < ::Rails::Railtie #:nodoc:
       initializer 'hocus_pocus.editor' do |app|
         ActiveSupport.on_load(:after_initialize) do
@@ -18,23 +32,8 @@ module HocusPocus
 
         ActiveSupport.on_load(:action_view) do
           if HocusPocus.config.enable_editor
-            module ::ActionView
-              class PartialRenderer
-                def render_partial_with_filename_caching
-                  (Thread.current[HocusPocus::Editor::PARTIAL_FILENAMES] ||= []) << @template unless @view.controller.class.name.starts_with?('HocusPocus::')
-                  render_partial_without_filename_caching
-                end
-                alias_method_chain :render_partial, :filename_caching
-              end
-
-              class TemplateRenderer
-                def render_template_with_filename_caching(template, layout_name = nil, locals = {})
-                  Thread.current[HocusPocus::Editor::VIEW_FILENAME] = template.virtual_path if @view.controller.try(:request).try(:format).try(:html?) && !@view.controller.class.name.starts_with?('HocusPocus::')
-                  render_template_without_filename_caching template, layout_name, locals
-                end
-                alias_method_chain :render_template, :filename_caching
-              end
-            end
+            ::ActionView::PartialRenderer.send :prepend, HocusPocus::Editor::PartialRendererExtension
+            ::ActionView::TemplateRenderer.send :prepend, HocusPocus::Editor::TemplateRendererExtension
           end
         end
       end
