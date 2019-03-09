44a56578e11b1faafd1bbcedddb84611c465af62
Akira Matsuda <ronnie@dio.jp>
2019-03-09 14:29:13 +0900

    "Asset was not declared to be precompiled in production."
    "Add `Rails.application.config.assets.precompile += %w( generator.css )` to `config/initializers/assets.rb` and restart your server"

--- engines/generator/lib/hocus_pocus/generator/engine.rb
+++ engines/generator/lib/hocus_pocus/generator/engine.rb
@@ -2,6 +2,11 @@ module HocusPocus
   module Generator
     class Engine < ::Rails::Engine
       isolate_namespace HocusPocus::Generator
+      initializer 'hocus_pocus.recorder.add middleware' do |app|
+        if HocusPocus.config.enable_generator
+          config.assets.precompile += %w(generator.css)
+        end
+      end
     end
   end
 end
